693e0ff92ff867631c2650deadb1f074f08b17e1
iamkorniichuk@gmail.com <iamkorniichuk@gmail.com>
2023-09-25 16:33:20 +0300

    Add Lullaby endpoints

--- lullabies_app/models.py
+++ lullabies_app/models.py
@@ -1,10 +1,14 @@
 from django.db import models
+from django.urls import reverse
 
 
 class Lullaby(models.Model):
     name = models.CharField(max_length=64)
     lyrics = models.TextField()
     url = models.URLField()
+
+    def get_absolute_url(self):
+        return reverse("lullaby-detail", kwargs={"pk": self.pk})
 
     def __str__(self):
         return f"Lullaby({self.name})"
