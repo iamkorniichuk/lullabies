ae7bef239462af75315bfce4a664158a496907e8
iamkorniichuk@gmail.com <iamkorniichuk@gmail.com>
2023-09-26 21:09:16 +0300

    Allow CORS all origins

--- lullabies/settings.py
+++ lullabies/settings.py
@@ -29,6 +29,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    "corsheaders",
     "lullabies_app",
     "partners",
     "contacts",
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
@@ -130,3 +132,5 @@ REST_FRAMEWORK = {
         "rest_framework.parsers.JSONParser",
     ],
 }
+
+CORS_ALLOW_ALL_ORIGINS = True
