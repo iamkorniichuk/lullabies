43dc0277d4170498306ddfb7182a21eb503f2bef
iamkorniichuk@gmail.com <iamkorniichuk@gmail.com>
2023-09-30 15:56:13 +0300

    Add valid Artist statistic

--- lullabies_app/views.py
+++ lullabies_app/views.py
@@ -7,7 +7,8 @@ from .models import Lullaby
 class LullabyStatisticView(APIView):
     def get(self, request, *args, **kwargs):
         data = Lullaby.objects.aggregate(
-            lullabies=Count("pk"), regions=Count("region", distinct=True)
+            lullabies=Count("pk"),
+            regions=Count("region", distinct=True),
+            artists=Count("artists", distinct=True),
         )
-        data["artists"] = 41  # TODO: Provide valid information
         return Response(data)
