4921698f473e14c47e978fc440feac56c7517ded
iamkorniichuk@gmail.com <iamkorniichuk@gmail.com>
2023-10-02 12:24:01 +0300

    Separate media & static storages

--- lullabies/settings.py
+++ lullabies/settings.py
@@ -136,8 +136,10 @@ AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
 AWS_ACCESS_KEY_ID = env("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = env("AWS_SECRET_ACCESS_KEY")
 
-STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+STATICFILES_STORAGE = "custom_storages.StaticFileStorage"
+STATICFILES_FOLDER = "static"
 
-MEDIAFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+MEDIAFILES_STORAGE = "custom_storages.MediaFileStorage"
+MEDIAFILES_FOLDER = "media"
 
 CORS_ALLOW_ALL_ORIGINS = True
