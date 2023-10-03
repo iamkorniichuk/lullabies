55551ea3f235c7eb0b9a43e40a054a45b2c1f4f2
iamkorniichuk@gmail.com <iamkorniichuk@gmail.com>
2023-10-03 11:47:26 +0300

    Add Partner logo for dark theme

--- partners/admin.py
+++ partners/admin.py
@@ -7,7 +7,8 @@ from .models import Partner
 
 @admin.register(Partner)
 class PartnerAdmin(admin.ModelAdmin):
-    list_display = ["pk", "name", "url_website", "img_logo"]
+    list_display = ["pk", "name", "url_website", "img_logo", "img_logo_dark"]
 
     url_website = a_tag_factory("website", "website")
     img_logo = img_tag_factory("logo", "logo")
+    img_logo_dark = img_tag_factory("logo dark theme", "logo_dark_theme")
--- partners/models.py
+++ partners/models.py
@@ -5,6 +5,10 @@ from django.urls import reverse
 class Partner(models.Model):
     name = models.CharField(max_length=64, unique=True)
     logo = models.ImageField(upload_to="logo")
+    logo_dark_theme = models.ImageField(
+        upload_to="logo-dark-theme",
+        default="img.freepik.com/free-vector/bird-colorful-logo-gradient-vector_343694-1365.jpg",
+    )
     website = models.URLField(unique=True)
 
     def get_absolute_url(self):
