ea0f0cbf4176081f25bfbf093b53a043c8e6f277
iamkorniichuk@gmail.com <iamkorniichuk@gmail.com>
2023-10-04 21:42:50 +0300

    Add MediaSource serializer

--- artists/serializers.py
+++ artists/serializers.py
@@ -3,7 +3,7 @@ from rest_framework import serializers
 from .models import Artist
 
 
-class NestedArtistSerializer(serializers.ModelSerializer):
+class ArtistSerializer(serializers.ModelSerializer):
     class Meta:
         model = Artist
         fields = "__all__"
--- lullabies_app/serializers.py
+++ lullabies_app/serializers.py
@@ -1,8 +1,19 @@
 from rest_framework import serializers
 
-from artists.serializers import NestedArtistSerializer
+from artists.serializers import ArtistSerializer
 
-from .models import Lullaby
+from .models import Lullaby, MediaSource
+
+
+class MediaSource(serializers.ModelSerializer):
+    class Meta:
+        model = MediaSource
+        fields = "__all__"
+
+    format = serializers.SerializerMethodField("get_format")
+
+    def get_format(self, obj):
+        return obj.format
 
 
 class LullabySerializer(serializers.ModelSerializer):
@@ -11,4 +22,5 @@ class LullabySerializer(serializers.ModelSerializer):
         fields = "__all__"
 
     region = serializers.CharField(source="get_region_display")
-    artists = NestedArtistSerializer(many=True)
+    artists = ArtistSerializer(many=True)
+    source = MediaSource()
