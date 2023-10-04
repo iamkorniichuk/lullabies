d5ee40350a168bad8b1c1f206245962eef420f28
iamkorniichuk@gmail.com <iamkorniichuk@gmail.com>
2023-10-04 21:31:35 +0300

    Add sending email swagger description

--- emails/views.py
+++ emails/views.py
@@ -1,6 +1,7 @@
 from rest_framework.views import APIView, Response
-from rest_framework.status import HTTP_400_BAD_REQUEST, HTTP_202_ACCEPTED
+from rest_framework.status import HTTP_400_BAD_REQUEST, HTTP_200_OK
 from rest_framework.parsers import MultiPartParser, FormParser
+from drf_yasg.utils import swagger_auto_schema
 
 from .serializers import EmailSerializer
 
@@ -8,12 +9,17 @@ from .serializers import EmailSerializer
 class SendEmailView(APIView):
     parser_classes = [MultiPartParser, FormParser]
 
+    @swagger_auto_schema(
+        request_body=EmailSerializer,
+        responses={
+            "200": "Email send",
+            "400": "Bad request",
+        },
+    )
     def post(self, request, *args, **kwargs):
         serializer = EmailSerializer(data=request.POST)
         if serializer.is_valid():
             serializer.send()
-            return Response(
-                {"message": "Email send succesfully"}, status=HTTP_202_ACCEPTED
-            )
+            return Response({"message": "Email send succesfully"}, status=HTTP_200_OK)
 
         return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
