bd59da19ae176f065046914ebab373c1391aaf13
iamkorniichuk@gmail.com <iamkorniichuk@gmail.com>
2023-10-04 20:37:19 +0300

    Describe format parameter in swagger

--- lullabies_app/viewsets.py
+++ lullabies_app/viewsets.py
@@ -1,9 +1,23 @@
 from rest_framework import viewsets
+from django.utils.decorators import method_decorator
+from drf_yasg.utils import swagger_auto_schema
+from drf_yasg import openapi
 
 from .serializers import LullabySerializer
 from .models import Lullaby, MediaSource
 
+source_format = openapi.Parameter(
+    "source-format",
+    openapi.IN_QUERY,
+    description="Available formats: `audio`, `video`",
+    required=False,
+    type=openapi.TYPE_STRING,
+)
 
+
+@method_decorator(
+    name="list", decorator=swagger_auto_schema(manual_parameters=[source_format])
+)
 class LullabyViewSet(viewsets.ReadOnlyModelViewSet):
     serializer_class = LullabySerializer
 
