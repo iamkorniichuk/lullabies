497fecbecf36df96dc268fd282d6664137f5bf66
iamkorniichuk@gmail.com <iamkorniichuk@gmail.com>
2023-10-04 21:39:30 +0300

    Add response example to statistic

--- statistic/views.py
+++ statistic/views.py
@@ -1,10 +1,26 @@
 from django.db.models import Count
 from rest_framework.views import APIView, Response
+from drf_yasg import openapi
+from drf_yasg.utils import swagger_auto_schema
 
 from lullabies_app.models import Lullaby
 
 
 class StatisticView(APIView):
+    @swagger_auto_schema(
+        responses={
+            "200": openapi.Response(
+                description="Amount statistic",
+                examples={
+                    "application/json": {
+                        "lullabies": "100",
+                        "regions": "13",
+                        "artists": "25",
+                    }
+                },
+            )
+        }
+    )
     def get(self, request, *args, **kwargs):
         data = Lullaby.objects.aggregate(
             lullabies=Count("pk"),
