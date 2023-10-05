96ae4577656b0abfb7f1463ecd1a2075c9cf37a7
iamkorniichuk@gmail.com <iamkorniichuk@gmail.com>
2023-10-05 15:16:25 +0300

    Fix not displaying nested serializer

--- media/serializers.py
+++ media/serializers.py
@@ -4,7 +4,7 @@ from drf_annotations.mixins import SerializeAnnotationsMixin
 from .models import MediaSource
 
 
-class MediaSource(SerializeAnnotationsMixin, serializers.ModelSerializer):
+class MediaSourceSerializer(SerializeAnnotationsMixin, serializers.ModelSerializer):
     class Meta:
         model = MediaSource
         fields = "__all__"
