e612be8ea2266838d27756075a85335563f5bc63
iamkorniichuk@gmail.com <iamkorniichuk@gmail.com>
2023-10-12 12:13:17 +0300

    Add translation fields to Region

--- lullabies_app/admin.py
+++ lullabies_app/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 
-from .models import Lullaby
+from .models import Lullaby, Region
 
 
 @admin.register(Lullaby)
 class LullabyAdmin(admin.ModelAdmin):
     list_display = ["pk", "name", "region", "source", "views"]
+
+
+@admin.register(Region)
+class RegionAdmin(admin.ModelAdmin):
+    list_display = ["pk", "name"]
