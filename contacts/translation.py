d0d323ac81e1a6662877bf922adccb456dedc8e2
iamkorniichuk@gmail.com <iamkorniichuk@gmail.com>
2023-11-16 13:04:36 +0200

    Fix: Remove translation Contact's name

--- contacts/translation.py
+++ contacts/translation.py
@@ -5,5 +5,5 @@ from .models import Contact
 
 @register(Contact)
 class ContactTranslation(TranslationOptions):
-    fields = ["name", "value"]
-    required_languages = {"default": ["name", "value"]}
+    fields = ["value"]
+    required_languages = {"default": ["value"]}
