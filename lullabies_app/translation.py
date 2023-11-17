eaecdc51c8d35a083c23dda692fc5814b30e1ab3
iamkorniichuk@gmail.com <iamkorniichuk@gmail.com>
2023-11-17 17:18:54 +0200

    Fix: Remove require parameter from Lullaby

--- lullabies_app/translation.py
+++ lullabies_app/translation.py
@@ -6,7 +6,6 @@ from .models import Lullaby, Region
 @register(Lullaby)
 class LullabyTranslation(TranslationOptions):
     fields = ["name", "lyrics"]
-    required_languages = {"default": ["name"]}
 
 
 @register(Region)
