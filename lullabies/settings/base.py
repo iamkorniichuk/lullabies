a360eddc29c8ebe67e2de5f74ebede4cf6465477
iamkorniichuk@gmail.com <iamkorniichuk@gmail.com>
2023-11-17 17:39:25 +0200

    Fix: Catch null env

--- lullabies/settings/base.py
+++ lullabies/settings/base.py
@@ -1,11 +1,18 @@
 from .env import env
 import json
 
+
+def json_loads_or_none(value):
+    if value is None:
+        return None
+    return json.loads(value)
+
+
 SECRET_KEY = env("SECRET_KEY")
 
 DEBUG = env("IS_DEVELOPMENT", cast=bool, default=True)
 
-ALLOWED_HOSTS = env("APP_HOSTS", cast=json.loads, default='["localhost"]')
+ALLOWED_HOSTS = env("APP_HOSTS", cast=json_loads_or_none, default=["localhost"])
 
 ROOT_URLCONF = "lullabies.urls"
 
@@ -23,7 +30,7 @@ MIDDLEWARE = [
 
 WSGI_APPLICATION = "lullabies.wsgi.application"
 
-cors_origins = env("CORS_ORIGINS", cast=json.loads, default=None)
+cors_origins = env("CORS_ORIGINS", cast=json_loads_or_none)
 if cors_origins:
     CORS_ALLOWED_ORIGINS = cors_origins
 else:
