0cb313be4b516907ede1ed8573f01faf426ffb1f
iamkorniichuk@gmail.com <iamkorniichuk@gmail.com>
2023-11-21 21:07:12 +0200

    Add lyrics formatting for all languages

--- lullabies_app/models/lullaby.py
+++ lullabies_app/models/lullaby.py
@@ -4,6 +4,7 @@ from django.conf import settings
 from django.utils.translation import gettext_lazy as _
 
 
+from modeltranslation.translator import translator
 from artists.models import Artist
 from media.models import MediaSource
 from commons.transliteration import register_transliteration
@@ -49,13 +50,18 @@ class Lullaby(models.Model):
     is_visible = models.BooleanField(default=True, verbose_name=_("is visible"))
 
     def save(self, *args, **kwargs):
-        field_name = f"lyrics_{settings.MODELTRANSLATION_DEFAULT_LANGUAGE}"
-        lyrics = getattr(self, field_name)
-        setattr(self, field_name, self.format_lyrics(lyrics))
+        options = translator.get_options_for_model(type(self))
+        for field in options.local_fields["lyrics"]:
+            field_name = f"lyrics_{field.language}"
+            lyrics = getattr(self, field_name)
+            setattr(self, field_name, self.format_lyrics(lyrics))
 
         return super().save(*args, **kwargs)
 
     def format_lyrics(self, lyrics):
+        if lyrics.isspace() or lyrics == "":
+            return ""
+
         original_lyrics = lyrics.split("\n")
         lyrics = []
         is_previous_line_space = False
