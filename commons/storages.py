1a53c3342521978a7b4a514a0c7dcd4b85b3855f
iamkorniichuk@gmail.com <iamkorniichuk@gmail.com>
2023-11-21 18:09:50 +0200

    Make media files private

--- commons/storages.py
+++ commons/storages.py
@@ -4,4 +4,6 @@ from storages.backends.s3boto3 import S3Boto3Storage
 
 class MediaFileStorage(S3Boto3Storage):
     location = settings.MEDIAFILES_FOLDER
+    default_acl = "private"
     file_overwrite = False
+    custom_domain = False
